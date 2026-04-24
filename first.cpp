7868a6b3a929b3e5ff91b69022170ed729076002
gulamkibria775 <gulamkibria775@gmail.com>
2026-04-24 13:35:32 +0600

    c++ introduction

---
+++ first.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float b = 2.41;
+    char c = 'a';
+    bool num = false;
+    long long d = 327298314793712;
+
+    cout << b << endl;
+    cout << c << endl;
+    cout << num << endl;
+    cout << d << endl;
+
+    return 0;
+}
